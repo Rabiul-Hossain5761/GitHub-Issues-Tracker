aec21f4ba406fdd01a4028437c65a0a6e29799a3
Rabiul-Hossain5761 <robiulk859@gmail.com>
2026-04-23 12:08:54 +0600

    Show open and closed issues from api

--- js/home.js
+++ js/home.js
@@ -59,3 +59,127 @@ const displayAllIssues = (Issues) => {
 
     
 };
+const loadOpenIssues = () => {
+    manageSpinn(true)
+    removeActive();
+    const clickBtn = document.getElementById("open-btn");
+    clickBtn.classList.add("Active");
+    fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
+    .then((res)=>res.json())
+    .then((json) => {
+        const openIssues = json.data.filter(issue => issue.status==="open");
+        displayOpenIssues(openIssues);
+    })
+}
+
+const displayOpenIssues = (issues) => {
+
+    const allOpenIssues = document.getElementById("Issues-container")
+    allOpenIssues.innerHTML="";
+
+    const allIssuescount = document.getElementById("totalCount");
+    allIssuescount.innerHTML="";
+    allIssuescount.innerHTML=`
+        <div class="flex items-center gap-3">
+        <img class="w-10 h-10 object-contain" src="./images/Aperture.png" alt="">
+        <div>
+        <h2>${issues.length} Issues</h2>
+        <p>Track and manage your project issues</p>
+        </div>
+        </div>
+        <div class="flex gap-5 items-center">
+        <h2>open</h2>
+        <h2>close</h2>
+        </div>`;
+
+
+
+    for(let issue of issues){
+        const card = document.createElement("div");
+        card.innerHTML=`
+        <div onclick ="loadModalData(${issue.id})" class="p-2 shadow-sm space-y-3 border-t-4 border-t-green-500">
+        <div class="flex justify-between">
+        <img class="object-contain" src="./images/Open-Status.png" alt="">
+        <button class="btn">${issue.priority}</button>
+
+        </div>
+        <h2 class="line-clamp-1">${issue.title}</h2>
+        <p class="line-clamp-2">The navigation menu doesn't collapse properly on mobile devices...</p>
+
+        <div class="flex gap-2 border-b-1 pb-2">
+         ${createElement(issue.labels)}
+        </div>
+
+        <div class="space-y-2">
+         <p>#1 by ${issue.author}</p>
+         <p>${issue.createdAt}</p>
+        </div>
+
+        </div>`;
+        allOpenIssues.append(card);
+    }
+    manageSpinn(false)
+}
+
+
+const loadClosedIssues = () => {
+    manageSpinn(true)
+    removeActive();
+    const clickBtn = document.getElementById("close-btn");
+    clickBtn.classList.add("Active");
+    fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
+    .then((res)=>res.json())
+    .then((json) => {
+        const closedIssues = json.data.filter(issue => issue.status==="closed");
+        displayClosedIssues(closedIssues);
+    })
+}
+
+const displayClosedIssues = (issues) => {
+
+    const allClosedIssues = document.getElementById("Issues-container")
+    allClosedIssues.innerHTML="";
+
+    const allIssuescount = document.getElementById("totalCount");
+    allIssuescount.innerHTML="";
+    allIssuescount.innerHTML=`
+        <div class="flex items-center gap-3">
+        <img class="w-10 h-10 object-contain" src="./images/Aperture.png" alt="">
+        <div>
+        <h2>${issues.length} Issues</h2>
+        <p>Track and manage your project issues</p>
+        </div>
+        </div>
+        <div class="flex gap-5 items-center">
+        <h2>open</h2>
+        <h2>close</h2>
+        </div>`;
+
+
+
+    for(let issue of issues){
+        const card = document.createElement("div");
+        card.innerHTML=`
+        <div onclick ="loadModalData(${issue.id})" class="p-2 shadow-sm space-y-3 border-t-4 border-t-purple-500">
+        <div class="flex justify-between">
+        <img class="object-contain" src="./images/Closed- Status .png" alt="">
+        <button class="btn">${issue.priority}</button>
+
+        </div>
+        <h2 class="line-clamp-1">${issue.title}</h2>
+        <p class="line-clamp-2">The navigation menu doesn't collapse properly on mobile devices...</p>
+
+        <div class="flex gap-2 border-b-1 pb-2">
+         ${createElement(issue.labels)}
+        </div>
+
+        <div class="space-y-2">
+         <p>#1 by ${issue.author}</p>
+         <p>${issue.createdAt}</p>
+        </div>
+
+        </div>`;
+        allClosedIssues.append(card);
+    }
+    manageSpinn(false)
+};
